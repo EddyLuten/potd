d7c5a6ee4867bee0dc75fde43245a02864f85f6e
Eddy Luten <eddyluten@gmail.com>
2015-01-22 23:38:02 -0800

    Solution to the POTD Int Overflow problem.

---
+++ 2015.01.21.int-overflow.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(void)
+{
+  int num = 1;
+
+  while (num > 0) // overflow wraps around
+  {
+    printf("%i\n", num);
+    num += num;
+  }
+
+  printf("Overflown: %i\n", num);
+  return 0;
+}
